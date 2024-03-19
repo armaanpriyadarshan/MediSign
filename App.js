a788140354c24741793c4803ee508b10e10442d6
sbcaram <sbcaram@gmail.com>
2024-03-19 11:24:16 -0400

    Changed Colors

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#071221',
+    color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },

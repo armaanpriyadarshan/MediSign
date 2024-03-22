1a632c52dcae377839d4b769c88bf103988f0cfa
sbcaram <sbcaram@gmail.com>
2024-03-22 12:15:33 -0400

    Co-authored-by: jaro488 <jaro488@users.noreply.github.com> Co-authored-by: armaan_priyadarshan@outlook.com <armaan_priyadarshan@outlook.com>

--- App.js
+++ App.js
@@ -1,23 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Header, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style = {styles.title}>Welcome to MediSign</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+          <Image
+            source={require('./assets/favicon.png')}
+          />
+          <Text style = {styles.title}>MediSign</Text>
+          <Text style = {styles.title}>Contact Us</Text>
+      </View>
     </View>
   );
 }
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.header}>
+      <Image
+          source={require('./assets/logo.svg')}
+        />
+        <Text style = {styles.title}>MediSign</Text>
+        <Text style = {styles.title}>Contact Us</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#071221',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#C0C0C0',
+    marginTop: 50
+  },
+  
   title: {
-    color: 'white',
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingHorizontal: 10
   }
 });
